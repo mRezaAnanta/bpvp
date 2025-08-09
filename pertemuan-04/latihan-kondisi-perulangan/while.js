d1cea3b5f305c7348c73b7964c82b8753392eec3
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-08-09 10:13:06 +0700

    feat(pertemuan-04): add pertemuan-04

--- pertemuan-04/latihan-kondisi-perulangan/while.js
+++ pertemuan-04/latihan-kondisi-perulangan/while.js
@@ -1,15 +1,25 @@
 let ulang = true
 
+// do {
+//   let i = 1
+//   console.log("Menampilkan bilangan genap dari 1 sampai 20:")
+//
+//   while (i <= 20) {
+//     if (i % 2 === 0) {
+//       console.log(i)
+//     }
+//     i++
+//   }
+//
+//   ulang = false
+// } while(ulang)
+
+let i = 1
+console.log("Menampilkan bilangan genap dari 1 sampai 20:")
+
 do {
-  let i = 1
-  console.log("Menampilkan bilangan genap dari 1 sampai 20:")
-
-  while (i <= 20) {
-    if (i % 2 === 0) {
-      console.log(i)
-    }
-    i++
+  if (i % 2 === 0) {
+    console.log(i)
   }
-
-  ulang = false
-} while(ulang)
+  i++
+} while (i <= 20)

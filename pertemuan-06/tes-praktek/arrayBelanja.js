b0c0126a78d8f60bc7332ffdeebccbcac6830746
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-08-11 09:01:42 +0700

    refactor(arraybelanja.js): improve function and legibility

--- pertemuan-06/tes-praktek/arrayBelanja.js
+++ pertemuan-06/tes-praktek/arrayBelanja.js
@@ -1,27 +1,26 @@
-let arr = [
+let daftar = [
   { nama: "New Balance NB530", harga: 390000 },
   { nama: "Nike Vomero 5", harga: 350000 },
   { nama: "Adidas Samba", harga: 180000 },
   { nama: "Onitsuka Tiger Mexico", harga: 420000 },
 ]
 
-console.log("====== Rincian Belanja Sepatu =====")
-arr.forEach((el, i) => {
-  console.log(`${i + 1}. ${el.nama} - ${el.harga}`)
-})
+const belanja = (pembayaran, daftar) => {
+  daftar.forEach((el, i) => {
+    console.log(`${i + 1}. ${el.nama} - ${el.harga}`)
+    total += el.harga
+  }, total = 0)
 
-let total = 0
-arr.map(x => x.harga).forEach(el => total += el)
-console.log(`Total Belanja = Rp. ${total}`)
+  let diskon = total < 250000 ? 0 : total < 500000 ? 5 : total < 800000 ? 10 : 15
+  let setelahDiskon = total - (total * diskon / 100)
+  let hasil = pembayaran - setelahDiskon
 
-let diskon = total < 250000 ? 0 : total < 500000 ? 5 : total < 800000 ? 10 : 15
-console.log(`Diskon = ${diskon}%`)
+  console.log("====== Rincian Belanja Sepatu =====")
+  console.log(`Total Belanja = Rp. ${total}`)
+  console.log(`Diskon = ${diskon}%`)
+  console.log(`Total Setelah Diskon = Rp. ${setelahDiskon}`)
+  console.log(`Pembayaran = Rp. ${pembayaran}`)
+  console.log(`Kembalian = Rp. ${hasil}`)
+}
 
-let setelahDiskon = total - (total * diskon / 100)
-console.log(`Total Setelah Diskon = Rp. ${setelahDiskon}`)
-
-let pembayaran = 1200000
-console.log(`Pembayaran = Rp. ${pembayaran}`)
-
-let hasil = pembayaran - setelahDiskon
-console.log(`Kembalian = Rp. ${hasil}`)
+belanja(1200000, daftar)

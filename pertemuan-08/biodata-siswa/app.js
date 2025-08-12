a5a846abf1a8df19a728ac40802b9fdf05d98070
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-08-12 14:41:00 +0700

    refactor(biodata-siswa/app.js): refactor app.js and add func so that it could iterate data.json

--- pertemuan-08/biodata-siswa/app.js
+++ pertemuan-08/biodata-siswa/app.js
@@ -5,15 +5,11 @@ const logikaKategori = require('./logic/logikaKategori.js')
 
 let siswa = [
   { nama: "Richard", umur: 30, asal: "Jakarta", sekolah: "Universitas X", tahunLulus: 2018},
+  { nama: "Sukri", umur: 10, asal: "ahsdsakdasld", sekolah: "Universitas X", tahunLulus: 20234},
 ]
 
 try {
-  let kategori = logikaKategori(siswa[0].tahunLulus)
-  tampilkanBiodata(siswa[0].nama, siswa[0].umur, siswa[0].asal, siswa[0].sekolah, siswa[0].tahunLulus, kategori, app.VERSION, app.AUTHOR)
-  // TODO: get data from the readAndWriteData func
-  // readAndWriteData(siswa)
-  // let logika = logikaKategori(siswa[0].tahunLulus)
-  // tampilkanBiodata(siswa[0].nama, siswa[0].umur, siswa[0].asal, siswa[0].sekolah, siswa[0].tahunLulus, kategori, app.VERSION, app.AUTHOR)
+  tampilkanBiodata(readAndWriteData(siswa), app.VERSION, app.AUTHOR)
 } catch (err) {
   console.log(err.message)
 }

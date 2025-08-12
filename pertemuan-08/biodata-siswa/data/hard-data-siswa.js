93f7c8d18ecc4e9673aa6e9b9a80f0389fa0392d
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-08-12 15:11:26 +0700

    feat(data/hard-data-siswa.js): add hard data so that the input will always stay the same

---
+++ pertemuan-08/biodata-siswa/data/hard-data-siswa.js
@@ -0,0 +1,6 @@
+const siswa = [
+  { nama: "Richard", umur: 30, asal: "Jakarta", sekolah: "Universitas X", tahunLulus: 2018},
+  { nama: "Sukri", umur: 10, asal: "ahsdsakdasld", sekolah: "Universitas X", tahunLulus: 20234},
+]
+
+module.exports = siswa

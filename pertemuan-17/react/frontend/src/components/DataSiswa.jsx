cacae7e5d528af9662ec91ae862c3bd610a036f9
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-08-24 11:42:40 +0700

    feat(pertemuan-17): implement feature to get data from database

--- pertemuan-17/react/frontend/src/components/DataSiswa.jsx
+++ pertemuan-17/react/frontend/src/components/DataSiswa.jsx
@@ -1,21 +1,45 @@
+import { useState, useEffect } from 'react'
+
 const DataSiswa = () => {
+  const [data, setData] = useState([])
+  const [keys, setKeys] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/siswa')
+        const data = await response.json()
+        const keys = Object.keys(data[0])
+        console.log(keys)
+        setData(data)
+        setKeys(keys)
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div>
       <p className="bg-red">Daftar Siswa</p>
       <table className="table-auto border border-collapse border-gray-400">
         <thead>
           <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>Umur</th>
+            { keys.map(el => (
+            <th>{el}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>1</th>
-            <th>Ahmad</th>
-            <th>50</th>
-          </tr>
+          { data.map(el => (
+            <tr>
+              <th>{el.id}</th>
+              <th>{el.nama}</th>
+              <th>{el.umur}</th>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
